2d36727534c5369625121f7a3a7df6f7ccd434e9
Aidan McCaughey <aidan.mccaughey9@gmail.com>
2026-04-02 01:17:00 +0100

    [#12 ] - add devices view code-behind

---
+++ src/InputAwareDisplaySwitcher.App/Views/DevicesView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace InputAwareDisplaySwitcher.App.Views;
+
+public partial class DevicesView : UserControl
+{
+    public DevicesView()
+    {
+        InitializeComponent();
+    }
+}
