3d30ba648bbe070f5605db953074804c526f2eb0
Aidan McCaughey <aidan.mccaughey9@gmail.com>
2026-04-02 01:17:01 +0100

    [#12 ] - add rules and settings view code-behind

---
+++ src/InputAwareDisplaySwitcher.App/Views/RulesSettingsView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace InputAwareDisplaySwitcher.App.Views;
+
+public partial class RulesSettingsView : UserControl
+{
+    public RulesSettingsView()
+    {
+        InitializeComponent();
+    }
+}
